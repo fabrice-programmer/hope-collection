645a855b079447694c3f814947a19c4e901ac55e
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-09 16:46:42 +0200

    h

--- fab.py
+++ fab.py
@@ -3,3 +3,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<h1> fab business </h1>'
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>this about of {username}</h1>'
