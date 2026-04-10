4fcfa1f8288331366682bc63d0c53942cdfc1a75
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-10 13:19:59 +0200

    yh

--- fab.py
+++ fab.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('home.html')
- 
+@app.route('/market')
+def market_page():
+     items= [
+        {"id": 1, "name": "phone", "barcode": "8673458", "price": 300},
+        {"id": 2, "name": "laptop", "barcode": "23455", "price": 800},
+        {"id": 3, "name": "keyboard", "barcode": "445544", "price": 50}
+    ]
+     return render_template('market.html', items=items)
+    
