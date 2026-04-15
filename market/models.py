76e5ff1333dd4ead0cce4fea734892a1ed423072
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-15 17:25:12 +0200

    yh

--- market/models.py
+++ market/models.py
@@ -1,4 +1,4 @@
-from app import db   
+from market import db
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -6,3 +6,8 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"Item{self.name}"
+
+    
--- market/run.py
+++ market/run.py
@@ -1,6 +1,4 @@
 from market import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-from market import routes
+if __name__=="__main__":
+ app.run(debug=True)
