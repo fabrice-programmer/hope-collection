dd2ba99c201b987347c0277159a22f6897b708c4
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-20 17:12:28 +0200

    y

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,11 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'mysecretkey'
 
 db = SQLAlchemy(app)
 
-from market import models
 from market import routes
--- market/models.py
+++ market/models.py
@@ -1,18 +1,17 @@
 from market import db
 
-class user(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email_address = db.column(db.string(length=50),nullable=False,unique=True)
-    password_hash = db.column(db.string(length=68),nullable=False)
-    budget = db.column(db.Integer(),nullable=False,)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email_address = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False, unique=True)
-    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    name = db.Column(db.String(30), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(1024), nullable=False)
+    barcode = db.Column(db.String(12), nullable=False)
+
 
     def __repr__(self):
         return f"Item('{self.name}')"
