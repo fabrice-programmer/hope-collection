c419ec610980ee726661a21139347a1afafe1687
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-24 12:19:21 +0200

    yh

--- market/__init__.py
+++ market/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = '6bbfe0ca18dacabb4c4f3b66'
 
 db = SQLAlchemy(app)
 
