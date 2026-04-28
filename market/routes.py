f06bafafc5841af0022dec80ed0b9cc5a8228b70
fabrice programmer <fabriceprogrammer@gmail.com>
2026-04-28 17:43:20 +0200

    yh

--- market/routes.py
+++ market/routes.py
@@ -3,17 +3,21 @@ from flask import render_template
 from market.models import Item
 from market.forms import RegisterForm
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
 def home_page():
     return render_template("home.html")
 
-@app.route("/market")
+from models import Market   # or your model name
+
+@app.route('/market')
 def market_page():
-    items = Item.query.all()
+    items = Market.query.all()   # fetch data from DB
     return render_template("market.html", items=items)
 
-@app.route("/register")
+@app.route('/login')
+def login_page():
+    return "Login Page"
+
+@app.route('/register')
 def register_page():
-    form = RegisterForm()
-    return render_template("register.html", form=form)
+    return "Register Page"
