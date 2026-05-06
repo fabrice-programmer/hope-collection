1d38304124e872f2f410054ba922283130107b07
fabrice programmer <fabriceprogrammer@gmail.com>
2026-05-06 09:52:35 +0200

    yh

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import bcrypt
 
 app = Flask(__name__)
 
@@ -7,6 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '6bbfe0ca18dacabb4c4f3b66'
 
 db = SQLAlchemy(app)
+bcrypt=bcrypt(app)
 
-# import routes ONLY after app + db are created
 from market import routes
