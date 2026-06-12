73caf0dc6bc4791cd9fe609f3d66e097599ff172
Fabrice Niyonsaba <fabriceprogrammer@gmail.com>
2026-06-12 11:28:15 +0200

    yh

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from market import db, app
+from market import app, db
 from market.models import *
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()
